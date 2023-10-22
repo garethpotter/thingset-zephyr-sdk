ff3db08828bc1005e6ca590b09abf2c07fa7b932
Gareth Potter <gareth.potter@brillpower.com>
2023-10-22 20:15:40 +0100

    remove twister output

--- twister-out.1/native_posix_64/can/thingset_sdk.can/zephyr/zephyr.dts.d
+++
@@ -1,9 +0,0 @@
-empty_file.o: /home/gaz/thingset-zephyr-sdk/zephyr/misc/empty_file.c \
- /home/gaz/thingset-zephyr-sdk/zephyr/boards/posix/native_posix/native_posix_64.dts \
- /home/gaz/thingset-zephyr-sdk/zephyr/boards/posix/native_posix/native_posix.dts \
- /home/gaz/thingset-zephyr-sdk/zephyr/dts/posix/posix.dtsi \
- /home/gaz/thingset-zephyr-sdk/zephyr/dts/common/skeleton.dtsi \
- /home/gaz/thingset-zephyr-sdk/zephyr/dts/common/mem.h \
- /home/gaz/thingset-zephyr-sdk/zephyr/include/zephyr/dt-bindings/adc/adc.h \
- /home/gaz/thingset-zephyr-sdk/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
- /home/gaz/thingset-zephyr-sdk/zephyr/include/zephyr/dt-bindings/gpio/gpio.h
--- twister-out.1/native_posix_64/isotp_fast/conformance_async/thingset_sdk.isotp_fast.conformance.async/zephyr/dev_handles.c
+++
@@ -1,8 +0,0 @@
-#include <zephyr/device.h>
-#include <zephyr/toolchain.h>
-
-/* 1 : /can_loopback0:
- */
-extern const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2"))) __devicehdl_dts_ord_4[3];
-const Z_DECL_ALIGN(device_handle_t) __attribute__((__section__(".__device_handles_pass2")))
-__devicehdl_dts_ord_4[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
